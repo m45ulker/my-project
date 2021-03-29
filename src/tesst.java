4f30aa3de4a5f968f261e70e70f93b3180068df6
m45ulker <bicer77@hotmail.com>
2021-03-29 11:24:55 -0500

    update 11.25

--- src/tesst.java
+++ src/tesst.java
@@ -1,6 +1,9 @@
 public class tesst {
     public static void main(String[] args) {
         for (int i = 0; i < 10 ; i++) {
+            if (true);
+            System.out.println("gooogle");
+
 
         }
     }
